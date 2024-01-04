1d8579b9014c71c338c9a431888981d2e07be153
Meu amor <meuamor@pop-os.localdomain>
2024-01-03 21:10:27 -0300

    changes

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -5,7 +5,7 @@ async function conectaBancoDeDados() {
     try {
         console.log('Conexão com o banco de dados iniciou')
 
-        await mongoose.connect(secrets.dbUrl)
+        await mongoose.connect(secrets.mongoUrl)
 
         console.log('Conexão com o banco de dados feita com sucesso!')
     } catch (erro) {
