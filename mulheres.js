b12e0e4069154f7ed32bc06c882d76e697486e44
Meu amor <meuamor@pop-os.localdomain>
2024-01-08 21:35:26 -0300

    WIP

--- mulheres.js
+++ mulheres.js
@@ -5,35 +5,20 @@ const { v4:uuidv4 } = require('uuid')
 const conectaBancoDeDados = require('./bancoDeDados') //ligando ao arquivo bancoDeDados
 conectaBancoDeDados() //chamando a função que conecta o banco de dados
 
+const Mulher = require('./mulherModel')
 const app = express() //aqui estou iniciando o app
 app.use(express.json())
 const porta = 3333 //criando a porta
 
-//criando lista inicial de mulheres 
-const mulheres = [
-    {
-        id: '1',
-        nome: 'Camila Ferrari',
-        imagem: 'https://avatars.githubusercontent.com/u/88910225?v=4',
-        minibio: 'Farmacêutica em transição de carreira para Desenvolvedora'
-    },
-    {
-        id: '2',
-        nome: 'Nicolle Meireles',
-        imagem: 'https://www.imagenspng.com.br/wp-content/uploads/2017/03/moana-01-920x1024.png',
-        minibio: 'Desenvolvedora e estudante de Computação'
-    },
-    {
-        id: '3',
-        nome: 'Ada Lovelace',
-        imagem: 'https://s2-techtudo.glbimg.com/7YOBBhghbMF9wsUuBP7UcUh2yXI=/0x342:2439x2307/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/n/3/FbrmyhQneVYWGUPIe8vA/adalovelace.jpg',
-        minibio: 'Matemática e programadora'
-    }
-]
-
 //GET
-function mostraMulheres(request, response) {
-    response.json(mulheres)
+async function mostraMulheres(request, response) {
+    try { 
+        const mulheresVindasDoBancoDeDados = await Mulher.find()
+        
+        response.json(mulheresVindasDoBancoDeDados)
+    } catch(erro) {
+        console.log(erro)
+    }
 }
 
 //POST
