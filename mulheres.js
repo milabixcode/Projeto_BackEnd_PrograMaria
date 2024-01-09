21aa6fabb73afb00e61429b9947d9973235cd40c
milabixcode <camila_ferrari@id.uff.br>
2024-01-08 23:40:19 -0300

    Banco de Dados

--- mulheres.js
+++ mulheres.js
@@ -1,6 +1,5 @@
 const express = require('express') //iniciando o express
 const router = express.Router() //configurando a primeira parte da rota
-const { v4:uuidv4 } = require('uuid')
 
 const conectaBancoDeDados = require('./bancoDeDados') //ligando ao arquivo bancoDeDados
 conectaBancoDeDados() //chamando a função que conecta o banco de dados
@@ -22,54 +21,61 @@ async function mostraMulheres(request, response) {
 }
 
 //POST
-function criaMulher(request, response) {
-    const novaMulher = {
-        id: uuidv4(),
+async function criaMulher(request, response) {
+    const novaMulher = new Mulher({
         nome: request.body.nome,
         imagem: request.body.imagem,
-        minibio: request.body.minibio
-    }
+        minibio: request.body.minibio,
+        citacao: request.body.citacao
+    })
 
-    mulheres.push(novaMulher)
-    response.json(mulheres)
+    try {
+        const mulherCriada = await novaMulher.save()
+        response.status(201).json(mulherCriada)
+    } catch(erro) {
+        console.log(erro)
+    }
 }
 
 //PATCH
-function editaMulher(request, response) {
-    function encontraMulher(mulher) {
-        if(mulher.id === request.params.id) {
-            return mulher
+async function editaMulher(request, response) {
+    try {
+        const mulherEncontrada = await Mulher.findById(request.params.id)
+
+        if(request.body.nome) {
+            mulherEncontrada.nome = request.body.nome
         }
+    
+        if(request.body.imagem) {
+            mulherEncontrada.imagem = request.body.imagem
+        }
+    
+        if(request.body.minibio) {
+            mulherEncontrada.minibio = request.body.minibio
+        }
+
+        if(request.body.citacao) {
+            mulherEncontrada.citacao = request.body.citacao
+        }
+
+        const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+        
+        response.json(mulherAtualizadaNoBancoDeDados)
+
+    }catch(erro){
+        console.log(erro)
     }
-
-    const mulherEncontrada = mulheres.find(encontraMulher)
-
-    if(request.body.nome) {
-        mulherEncontrada.nome = request.body.nome
-    }
-
-    if(request.body.imagem) {
-        mulherEncontrada.imagem = request.body.imagem
-    }
-
-    if(request.body.minibio) {
-        mulherEncontrada.minibio = request.body.minibio
-    }
-
-    response.json(mulheres)
 }
 
 //DELETE
-function deletaMulher(request, response) {
-    function todasMenosEla(mulher) {
-        if(mulher.id !== request.params.id) {
-            return mulher
-        }
+async function deletaMulher(request, response) {
+    try {
+        await Mulher.findByIdAndDelete(request.params.id)
+        response.json({mensagem: 'Mulher deletada com sucesso!'})
+
+    } catch(erro) {
+        console.log(erro)
     }
-
-    const mulheresQueFicam = mulheres.filter(todasMenosEla)
-
-    response.json(mulheresQueFicam)
 }
 
 app.use(router.get('/mulheres', mostraMulheres)) //configurando rota GET /mulheres
